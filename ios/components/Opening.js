cde2b428294fd32677a8ed359e591a17695e5b92
Bradneal14 <bradleyhneal@gmail.com>
2016-08-17 14:50:59 -0700

    adds button on last slide which will take user to dash

--- ios/components/Opening.js
+++ ios/components/Opening.js
@@ -3,6 +3,7 @@ import {
   StyleSheet,
   Text,
   View,
+  TouchableHighlight
 } from 'react-native';
 
 import Swiper from 'react-native-swiper'
@@ -12,17 +13,33 @@ class Opening extends Component{
     super(props);
     this.state = {}
   }
+  sendUserToDash(){
+    console.log("sending user to dash");
+  }
   render(){
     return (
-     <Swiper style={styles.wrapper} showsButtons={true} loop={false}>
+     <Swiper style={styles.wrapper} showsButtons={false} loop={false}>
        <View style={styles.slide1}>
-         <Text style={styles.text}>Hello Swiper</Text>
+         <Text style={styles.text}>Welcome to Reflect</Text>
        </View>
        <View style={styles.slide2}>
-         <Text style={styles.text}>Beautiful</Text>
+         <Text style={styles.text}>Step 1: Pick Questions</Text>
        </View>
        <View style={styles.slide3}>
-         <Text style={styles.text}>And simple</Text>
+         <Text style={styles.text}>Step 2: Log Answers</Text>
+       </View>
+       <View style={styles.slide4}>
+         <View style={styles.topHalf}>
+           <Text style={styles.text}>Step 3: See Results</Text>
+         </View>
+         <View style={styles.bottomHalf}>
+           <TouchableHighlight
+             style={styles.button}
+             underlayColor={"#ff1493"}
+             onPress={this.sendUserToDash}>
+              <Text style={styles.buttonText}>Touch Here</Text>
+            </TouchableHighlight>
+          </View>
        </View>
      </Swiper>
    )
@@ -50,10 +67,34 @@ var styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#92BBD9',
   },
+  slide4: {
+    flex: 1,
+    backgroundColor: '#92BBD9',
+  },
   text: {
     color: '#fff',
     fontSize: 30,
     fontWeight: 'bold',
+  },
+  topHalf:{
+    flex: 1,
+    alignItems: "center",
+    justifyContent: 'flex-end'
+  },
+  bottomHalf: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 100,
+    width: 300,
+  },
+  buttonText: {
+    fontSize: 30,
+    color: 'green'
   }
 })
 
