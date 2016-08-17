2cc185a28eed456d9271e34bbd201b2a916125be
Bradneal14 <bradleyhneal@gmail.com>
2016-08-17 14:30:54 -0700

    successfully installs basic working swiper with its accompanying styles

--- ios/components/Opening.js
+++ ios/components/Opening.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import {
+  StyleSheet,
   Text,
   View,
 } from 'react-native';
+
+import Swiper from 'react-native-swiper'
 
 class Opening extends Component{
   constructor(props){
@@ -10,11 +13,49 @@ class Opening extends Component{
     this.state = {}
   }
   render(){
-    return(
-      <Text>Hello</Text>
-    )
+    return (
+     <Swiper style={styles.wrapper} showsButtons={true} loop={false}>
+       <View style={styles.slide1}>
+         <Text style={styles.text}>Hello Swiper</Text>
+       </View>
+       <View style={styles.slide2}>
+         <Text style={styles.text}>Beautiful</Text>
+       </View>
+       <View style={styles.slide3}>
+         <Text style={styles.text}>And simple</Text>
+       </View>
+     </Swiper>
+   )
   }
 }
+
+var styles = StyleSheet.create({
+  wrapper: {
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  }
+})
 
 Opening.external=true;
 module.exports = Opening;
