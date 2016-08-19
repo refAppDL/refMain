12b1c66192c9d70ee7e04b005ce8c6c84aa5611f
Godsten Silver <geeausten@gmail.com>
2016-08-19 16:39:23 -0700

    Page and Body in the Mix

--- ios/components/Body.js
+++ ios/components/Body.js
@@ -1,2 +1,51 @@
-var Dash = require("./Dash.js")
-var Survery = require("/.Survey.js")
+var Dash = require("./Dash.js");
+var Survey = require("/.Survey.js");
+
+import React, { Component } from 'react';
+import {
+  Text,
+  View,
+  StyleSheet
+} from 'react-native';
+
+class Body extends Component {
+  constructor(props){
+    super(props);
+    this.state={goToSurvey: true};
+
+  }
+  toSurvey(e){
+    e.preventDefault();
+    this.setState({goToSurvey: true});
+  }
+  render(){
+    var pageKeep;
+    if(goToSurvey)
+      pageKeep = <Survey />
+    else
+      pageKeep = <Dash goToQuestions={this.toSurvey.bind(this)} />
+    return(
+      <View style={styles.wrapper}>
+        {pageKeep}
+      </View>
+    )
+  }
+}
+
+var styles = StyleSheet.create({
+  wrapper: {
+    flex: 1,
+    backgroundColor: '#fff00f',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 40,
+    color: "#00f0ff"
+  }
+
+})
+
+
+Body.external = true;
+module.exports = Body;
