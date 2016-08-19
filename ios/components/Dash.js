b7e80b9fd59ae930dff7eab40db0e2f8592684b1
Godsten Silver <geeausten@gmail.com>
2016-08-19 14:05:29 -0700

    added Dash

--- ios/components/Dash.js
+++ ios/components/Dash.js
@@ -1,2 +1,77 @@
 var Results = require("./Results.js");
 var QuestionManager = require("./QuestionManager");
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight
+} from 'react-native';
+
+
+class Dash extends Component{
+  constructor(props){
+    super(props);
+    this.state = {resultsSelected: true};
+
+  }
+
+  resultsSelect(event){
+    event.preventDefault();
+    if(!this.state.resultsSelected){
+      this.setState({resultsSelected: true});
+    }
+  }
+  questionsSelect(event){
+    event.preventDefault();
+    if(this.state.resultsSelected){
+      this.setState({resultsSelected: false});
+    }
+  }
+
+
+  render(){
+
+    var pageShow, resultsButtonClass, questionsButtonClass;
+    if(resultsSelected){
+      pageShow = <Results />
+      resultsButtonClass= "selected";
+      questionsButtonClass = "unselected";
+    }else{
+      pageShow = <QuestionManager />
+      resultsButtonClass = "selected";
+      questionsButtonClass = "unselected";
+
+    }
+    return(
+      <View>
+        <TouchableHighlight
+          className={resultsButtonClass}
+          onClick={this.resultsSelect}>
+            Results
+        </TouchableHighlight>
+        <TouchableHighlight
+          className={questionsButtonClass}
+          onClick={this.questionsSelect}>
+            Manage Questions
+        </TouchableHighlight>
+        {pageShow}
+      </View>
+    )
+  }
+}
+var styles = StyleSheet.create({
+  selected: {
+    color: 'blue',
+    alignSelf: 'center',
+    flexDirection: 'row'
+  },
+  unselected: {
+    color: 'yellow',
+    alignSelf: 'center'
+  }
+});
+Dash.external = true;
+module.exports = Dash;
---
+++ ios/components/Results.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  View,
+  Text,
+  StyleSheet,
+  TouchableHighlight
+} from 'react-native';
+
+
+class Results extends Component{
+  constructor(props){
+    super(props);
+    this.state = {};
+
+  }
+
+
+  render(){
+
+    return(
+      <View>
+        <Text>This is where you see your Results</Text>
+      </View>
+    )
+  }
+}
+var styles = StyleSheet.create({
+  main: {
+    color: 'blue',
+    alignSelf: 'center',
+    flexDirection: 'row'
+  }
+});
+Results.external = true;
+module.exports = Results;
