969a7083fdf23e0aeb9ae8d19919d5b8c125ea28
Godsten Silver <geeausten@gmail.com>
2016-08-22 11:35:23 -0700

    fixed navigation

--- ios/components/Dash.js
+++ ios/components/Dash.js
@@ -1,5 +1,8 @@
 var Results = require("./Results.js");
 var QuestionManager = require("./QuestionManager");
+var Button  = require('react-native-button');
+var Modal   = require('react-native-modalbox');
+// var Slider  = require('react-native-slider');
 
 import React, { Component } from 'react';
 import {
@@ -14,8 +17,22 @@ import {
 class Dash extends Component{
   constructor(props){
     super(props);
-    this.state = {resultsSelected: true};
+    this.state = {resultsSelected: true,
+        isOpen: true,
+        isDisabled: false,
+        swipeToClose: true,
+        sliderValue: 0.3};
 
+  }
+  openModal1(id) {
+   this.setState({isOpen:false});
+  }
+  toggleDisable() {
+    this.setState({isDisabled: !this.state.isDisabled});
+  }
+
+  toggleSwipeToClose() {
+    this.setState({swipeToClose: !this.state.swipeToClose});
   }
 
   resultsSelect(event){
@@ -37,45 +54,100 @@ class Dash extends Component{
     var pageShow, resultsButtonClass, questionsButtonClass;
     if(this.state.resultsSelected){
       pageShow = <Results />
-      resultsButtonClass= "selected";
-      questionsButtonClass = "unselected";
+      resultsButtonStyle = styles.rSelected;
+      questionsButtonStyle = styles.qUnselected;
     }else{
       pageShow = <QuestionManager />
-      resultsButtonClass = "selected";
-      questionsButtonClass = "unselected";
+      resultsButtonStyle = styles.rUnselected;
+      questionsButtonStyle = styles.qSelected;
 
     }
     return(
       <View>
-        <View className="resultsButton">
+          <Button onPress={this.openModal1.bind(this)} style={styles.btn}>Basic modal</Button>
+          <Modal style={[styles.modal, styles.modal1]} ref={"modal1"} swipeToClose={this.state.swipeToClose} onClosed={this.onClose} onOpened={this.onOpen} onClosingState={this.onClosingState}>
+            <Text style={styles.text}>Basic modal</Text>
+            <Button onPress={this.toggleSwipeToClose} style={styles.btn}>Disable swipeToClose({this.state.swipeToClose ? "true" : "false"})</Button>
+          </Modal>
           <TouchableHighlight
-            className={resultsButtonClass}
+            style={resultsButtonStyle}
             onPress={this.resultsSelect.bind(this)}>
               <Text>Results</Text>
           </TouchableHighlight>
-        </View>
-        <View className="QButton">
           <TouchableHighlight
-            className={questionsButtonClass}
+            style={questionsButtonStyle}
             onPress={this.questionsSelect.bind(this)}>
               <Text>Manage Questions</Text>
           </TouchableHighlight>
-        </View>
         {pageShow}
       </View>
     )
   }
 }
 var styles = StyleSheet.create({
-  selected: {
-    color: 'blue',
-    alignSelf: 'center',
-    flexDirection: 'row'
+  rSelected: {
+    backgroundColor: 'blue',
+    height: 80,
+    width: 80,
+    borderColor: 'black',
+    borderTopLeftRadius: 40,
+    borderBottomLeftRadius: 40,
+    justifyContent: 'center'
+    // position: 'absolute',
+    // top: 30,
+
   },
-  unselected: {
-    color: 'yellow',
-    alignSelf: 'center'
+  rUnselected: {
+    backgroundColor: 'red',
+    height: 80,
+    width: 80,
+    borderColor: 'black',
+    borderTopLeftRadius: 40,
+    borderBottomLeftRadius: 40,
+    justifyContent: 'center'
+  },
+  qSelected: {
+    backgroundColor: 'blue',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 80,
+    width: 80,
+    borderColor: 'black',
+    borderTopRightRadius: 40,
+    borderBottomRightRadius: 40
+  },
+  qUnselected: {
+    backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 80,
+    width: 80,
+    borderTopRightRadius: 40,
+    borderBottomRightRadius: 40
+  },
+  modal: {
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  btn: {
+    margin: 10,
+    backgroundColor: "#3B5998",
+    color: "white",
+    padding: 10
+  },
+  btnModal: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    width: 50,
+    height: 50,
+    backgroundColor: "transparent"
+  },
+  text: {
+    color: "black",
+    fontSize: 22
   }
+
 });
 Dash.external = true;
 module.exports = Dash;
