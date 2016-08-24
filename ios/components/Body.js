fb667a8e8f79d3cabdc20f41ae4b1553a538ad86
Bradneal14 <bradleyhneal@gmail.com>
2016-08-23 23:00:48 -0700

    adds the working survey component, survey asks all questions, records answers to state and routes back to the lounge when complete

--- ios/components/Body.js
+++ ios/components/Body.js
@@ -13,17 +13,36 @@ class Body extends Component {
     super(props);
     this.state={
       goToSurvey: false,
-      destination: this.props.destination,
+      destination: "survey",
       numberOfActive: 3,
       numberOfRetired: 4,
       activeQuestions: ["did you eat today?", "did you drink today?", "did you breathe today?"],
-      results: []
+      results: [],
+      shownAll: false,
+      currentlyShowing: 0
     };
+
+  }
+  getResultFromUser(result){
+    this.setState({results: this.state.results.concat(result)});
 
   }
   toSurvey(e){
     e.preventDefault();
     this.setState({goToSurvey: true, destination: 'survey'});
+  }
+  goToLounge(){
+    this.setState({goToSurvey: false, destination: 'lounge'})
+  }
+  showNextSurveyQuestion(){
+    if (this.state.currentlyShowing < this.state.numberOfActive - 1){
+      this.setState({currentlyShowing: this.state.currentlyShowing + 1});
+    } else {
+      this.setState({shownAll: true})
+    }
+  }
+  doNothing(){
+    console.log("nada");
   }
   getStateFromAsync(){
     AsyncStorage.getItem('appData').then(value=>{
@@ -42,12 +61,26 @@ class Body extends Component {
   }
   render(){
     var display;
-    if(this.state.destination === 'survey')
-      display = <Survey /> //Need to pass question props
-    else
+    if(this.state.destination === 'survey'){
+      if (!this.state.shownAll){
+         display = <Survey
+           getResultFromUser={this.getResultFromUser.bind(this)}
+           nextSurvey={this.showNextSurveyQuestion.bind(this)}
+           currentQuestion={this.state.activeQuestions[this.state.currentlyShowing]}
+           questionIndex={this.state.currentlyShowing}
+           questionTotal={this.state.numberOfActive}/>
+       } else {
+         display = <Survey
+           getResultFromUser={this.doNothing.bind(this)}
+           nextSurvey={this.goToLounge.bind(this)}
+           currentQuestion={"NO MORE QUESTIONS. ANSWERS: " + this.state.results.toString()}
+           />
+       }
+    } else {
       display = <Dash
                   goToQuestions={this.toSurvey.bind(this)}
                   destination={this.props.destination}/>
+              }
     return(
       <View style={styles.wrapper}>
         {display}
